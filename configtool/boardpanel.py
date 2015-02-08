b3fb95969d8282cf0e9a068262c828011ae7d7ff
jbernardis <jeff.bernardis@gmail.com>
2015-02-07 23:38:12 -0500

    Configtool: some code prettifications.
    Pure whitespace changes.

--- configtool/boardpanel.py
+++ configtool/boardpanel.py
@@ -444,6 +444,7 @@ class BoardPanel(wx.Panel):
                              "File error", wx.OK + wx.ICON_ERROR)
       dlg.ShowModal()
       dlg.Destroy()
+
       return False
 
     self.configFile = path
