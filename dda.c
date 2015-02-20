sjas/Teacup_Firmware

commit 95926a3f113809bde8ff0c84b94c55c73e398f67

DDA: Rename confusing variable name.
'all_time' sounds like forever to me, but this variable really
tracks the last time we hit one of "all the axes".  It sticks
out more now in looping, so rename it to make sense.

dda.c
@@ -651,7 +651,7 @@ void dda_step(DDA *dda) {
 		dda->c = 0xFFFFFFFF;
     for (i = X; i < AXIS_COUNT; i++) {
       if (move_state.steps[i]) {
-        c_candidate = move_state.time[i] + dda->step_interval[i] - move_state.all_time;
+        c_candidate = move_state.time[i] + dda->step_interval[i] - move_state.last_time;
         if (c_candidate < dda->c) {
           dda->axis_to_step = i;
           dda->c = c_candidate;

dda.h
@@ -71,8 +71,8 @@ typedef struct {
 	uint32_t					step_no;
 	#endif
 	#ifdef ACCELERATION_TEMPORAL
-  axes_uint32_t     time;   ///< time of the last step on each axis
-	uint32_t					all_time; ///< time of the last step of any axis
+  axes_uint32_t     time;       ///< time of the last step on each axis
+  uint32_t          last_time;  ///< time of the last step of any axis
 	#endif
 
 	/// Endstop handling.

commit 16cf39ebbf6ed33bc09372e1cde50d9c716b536e

dda.c: complete the rename from all_step to last_step.
Next time, please at least try to compile the code section in
question when explicitely changing the section. In this case,
with ACCELERATION_TEMPORAL enabled. It didn't build.

Was broken with commit 95926a3f113809bde8ff0c84b94c55c73e398f67,
"DDA: Rename confusing variable name.".

dda.c
@@ -570,7 +570,7 @@ void dda_step(DDA *dda) {
 		x_step();
     move_state.steps[X]--;
     move_state.time[X] += dda->step_interval[X];
-    move_state.all_time = move_state.time[X];
+    move_state.last_time = move_state.time[X];
 	}
 #endif
 
@@ -588,7 +588,7 @@ void dda_step(DDA *dda) {
 		y_step();
     move_state.steps[Y]--;
     move_state.time[Y] += dda->step_interval[Y];
-    move_state.all_time = move_state.time[Y];
+    move_state.last_time = move_state.time[Y];
 	}
 #endif
 
@@ -606,7 +606,7 @@ void dda_step(DDA *dda) {
 		z_step();
     move_state.steps[Z]--;
     move_state.time[Z] += dda->step_interval[Z];
-    move_state.all_time = move_state.time[Z];
+    move_state.last_time = move_state.time[Z];
 	}
 #endif
 
@@ -624,7 +624,7 @@ void dda_step(DDA *dda) {
 		e_step();
     move_state.steps[E]--;
     move_state.time[E] += dda->step_interval[E];
-    move_state.all_time = move_state.time[E];
+    move_state.last_time = move_state.time[E];
 	}
 #endif
 